1b993f962159868730943de78dd359ef66ee5917
Kai Marshland <kaimarshland@gmail.com>
2016-04-23 00:18:44 -0700

    Fixed bug where it was trying to boot habmc rather than spaceatstanford

--- start_puma.sh
+++ start_puma.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source ~/.bashrc
 source ~/.profile
-cd /var/habmc && /root/.rbenv/shims/bundle exec puma -C config/puma.rb
+cd /var/spaceatstanford && /root/.rbenv/shims/bundle exec puma -C config/puma.rb
