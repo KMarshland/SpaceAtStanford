fae24b5b81bd477e2e7c6d39dadd98848b3add3f
Kai Marshland <kaimarshland@gmail.com>
2016-05-04 02:53:01 -0700

    Fixed database

--- start_puma.sh
+++ start_puma.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-source ~/.bashrc
-source ~/.profile
-cd /var/spaceatstanford && /root/.rbenv/shims/bundle exec puma -C config/puma.rb
+printf "Trying to boot the bastard\n" >> /home/ubuntu/spaceatstanford/log/upstart.log
+source /home/ubuntu/.bashrc
+source /home/ubuntu/.profile
+source /home/ubuntu/.bash_profile
+cd /home/ubuntu/spaceatstanford && /home/ubuntu/.rbenv/shims/bundle exec /home/ubuntu/.rbenv/shims/puma -C config/puma.rb -e production
